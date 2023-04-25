a0aa42d55e98643725a0126bbf9e000379383bdb
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-25 14:00:41 +0200

    Receive new data and save to ingredient array which is then passed to ingredient form component

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      {id: Math.random().toString(), ...ingredient}
+    ])
+  }
+
+  const onRemoveItem = () => {
+    console.log('Remove')
+    setUserIngredients((userIngredients, id) => {
+      const idIndex = userIngredients.findIndex((obj) => obj.id === id);
+      if (idIndex > -1) {
+        userIngredients.splice(idIndex, 1);
+      }
+      return userIngredients
+    })
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm addIngredientHandler={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={onRemoveItem}/>
       </section>
     </div>
   );
