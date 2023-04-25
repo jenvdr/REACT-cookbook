45676e136f1e001388f4340c69b621deef69add5
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-25 13:58:56 +0200

    Capture entered Input value in state and pass to parent component

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+  const [inputState, setInputState ] = useState({title: '', amount: ''});
+
+  const inputChangeHandler = event => {
+    if (event.target.id === "title") {
+      setInputState({...inputState, title: event.target.value})
+    } else {
+      setInputState({...inputState, amount: event.target.value})
+    }
+  }
+
+  const onSubmitHandler = event => {
     event.preventDefault();
-    // ...
+    props.addIngredientHandler(inputState);
   };
 
   return (
     <section className="ingredient-form">
       <Card>
-        <form onSubmit={submitHandler}>
+        <form onSubmit={onSubmitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputState.title} onChange={inputChangeHandler}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={inputState.amount} onChange={inputChangeHandler} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
