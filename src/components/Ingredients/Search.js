82b06fa90e4a947e26717bf357a275c458f80ea5
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-25 17:32:16 +0200

    Remove handler function for change event and change to inline

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,40 +6,46 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onFilterItemsBySearch } = props;
   const [enteredSearch, setEnteredSearch] = useState('');
-
-  const searchHandler = event => {
-    setEnteredSearch(event.target.value);
-  }
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredSearch.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredSearch}"`;
-    fetch('https://react-cookbook-e3ca4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query, {
-      method: 'GET',
-    }).then(response => {
-      if (!response.ok) {
-        return;
-      }
-      return response.json();
-    }).then(responseData => {
+    const timer = setTimeout(() => {
+        if (enteredSearch === inputRef.current.value) {
+          const query = enteredSearch.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredSearch}"`;
+          fetch('https://react-cookbook-e3ca4-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query, {
+            method: 'GET',
+          }).then(response => {
+            if (!response.ok) {
+              return;
+            }
+            return response.json();
+          }).then(responseData => {
 
-      const loadedIngredients = [];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      props.onFilterItemsBySearch(loadedIngredients);
-    });
-  }, [enteredSearch, onFilterItemsBySearch])
+            const loadedIngredients = [];
+            for(const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            props.onFilterItemsBySearch(loadedIngredients);
+          });
+        }
+    }, 500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+
+  }, [enteredSearch, onFilterItemsBySearch, inputRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredSearch} onChange={searchHandler}/>
+          <input ref={inputRef} type="text" value={enteredSearch} onChange={event => setEnteredSearch(event.target.value)}/>
         </div>
       </Card>
     </section>
