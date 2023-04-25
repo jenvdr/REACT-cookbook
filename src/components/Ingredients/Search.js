eb5dc38be15a332c64ef6af1860664b8d0cfcca4
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-25 17:44:28 +0200

    Remove unused hook and disbale eslint for dependencies

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -38,6 +38,7 @@ const Search = React.memo(props => {
       clearTimeout(timer)
     }
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [enteredSearch, onFilterItemsBySearch, inputRef])
 
   return (
